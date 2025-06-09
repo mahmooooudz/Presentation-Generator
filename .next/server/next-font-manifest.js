9c4b9e528763735a8beb87195fb99af53d7eabc4
Mahmoud Emad <63793020+mahmooooudz@users.noreply.github.com>
2025-06-09 17:08:19 +0300

    Updated build-manifest.json

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/app/layout\":[\"static/media/ef46db3751d8e999-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
